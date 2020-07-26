01f299edde752b43e12c6f26c0aadba3695685d2
unknown <abdulrehman.6ab@gmail.com>
2020-07-26 08:15:03 +0500

    'first'

---
+++ js/script.js
@@ -0,0 +1,52 @@
+var popupClose=document.querySelector('.popupCross'),
+    menu=document.querySelector('.hamburgerMenu'),
+    nav=document.querySelector('.header__top__nav');
+
+
+popupClose.onclick=function () {
+    document.querySelector('.topPopup').style.display='none';
+};
+
+menu.onclick=function () {
+    nav.classList.toggle('hideNav');
+};
+
+window.addEventListener('click',function (e) {
+
+
+    if( !(nav.contains(e.target)) && e.target!==nav && e.target!==menu && !(menu.contains(e.target))){
+        nav.classList.add('hideNav');
+    }
+
+
+});
+
+
+window.onload=function () {
+
+    if(window.innerWidth<=760){
+
+
+        document.querySelectorAll('.dropbtn').forEach(ele=>{
+
+            ele.nextElementSibling.classList.add('hideNavDropdown');
+
+            ele.addEventListener('click',()=>{
+                ele.nextElementSibling.classList.toggle('hideNavDropdown');
+            })
+
+
+        })
+
+
+    }
+
+
+    let year=(new Date()).getFullYear();
+    document.querySelector('.footerYear').innerHTML=year;
+
+
+
+};
+
+
